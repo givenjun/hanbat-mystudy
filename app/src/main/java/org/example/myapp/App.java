f48f37ca82e937004637322de0321e48c7ea94f3
givenjun <wjdwns0531@gmail.com>
2025-07-30 15:16:39 +0900

    IntelliJ 환경설정

--- app/src/main/java/org/example/myapp/App.java
+++ app/src/main/java/org/example/myapp/App.java
@@ -1,19 +1,13 @@
 package org.example.myapp;
 
-import com.google.common.base.Joiner;
-import org.example.util.Utils;
+import java.util.Scanner;
 
-/** 이 클래스는 메인 애플리케이션이다. */
 public class App {
-  /**
-   * 애플리케이션 entry point 이다.
-   *
-   * @param args 프로그램 아규먼트
-   * @see <a href="https://docs.oracle.com/en/java/javase/21/">JDK Documentation</a>
-   */
   public static void main(String[] args) {
-    Utils.printMessage("hello!");
-    String message = Joiner.on(", ").join("hello", "world");
-    System.out.println(message);
+    Scanner scanner = new Scanner(System.in);
+    System.out.print("이름? ");
+    String name = scanner.next();
+    System.out.printf("안녕하세요 %s 님!\n", name);
+    scanner.close();
   }
 }
